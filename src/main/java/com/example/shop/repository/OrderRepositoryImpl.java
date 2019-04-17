3dcef8aa73e443634581835cfc790a0cd336cbee
Pawel Szczepkowski <pawel.szczepkowski@ttc.com>
2019-04-17 05:16:33 +0100

    remove order repo save .. additional implementation

--- src/main/java/com/example/shop/repository/OrderRepositoryImpl.java
+++ src/main/java/com/example/shop/repository/OrderRepositoryImpl.java
@@ -1,11 +1,7 @@
 package com.example.shop.repository;
 
 import com.example.shop.entity.Order;
-import com.example.shop.entity.OrderLineItem;
 import org.springframework.stereotype.Repository;
-
-import javax.transaction.Transactional;
-import java.util.Arrays;
 
 @Repository
 public class OrderRepositoryImpl extends AbstractRepository<Order> implements OrderRepository
@@ -14,12 +10,5 @@ public class OrderRepositoryImpl extends AbstractRepository<Order> implements Or
         setClazz(Order.class);
     }
 
-    @Override
-    @Transactional
-    public Order save(Order order) {
-        OrderLineItem orderLineItem = new OrderLineItem();
-        order.setOrderLineItemList(Arrays.asList(orderLineItem));
-        orderLineItem.setOrder(order);
-       return super.save(order);
-    }
+
 }
